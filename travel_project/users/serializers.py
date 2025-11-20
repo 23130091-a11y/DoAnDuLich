f72982b68df50d14f0dded166452afb09af683e3
itlord23 <nguyenthihonghanh5098@gmail.com>
2025-11-20 22:47:37 +0700

    tâm

--- travel_project/users/serializers.py
+++ travel_project/users/serializers.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
-from .models import User
-
+from .models import User, TravelPreference
 
 class UserSerializer(serializers.ModelSerializer) :
+    #
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['id', 'email', 'password']
+        fields = ('id', 'username', 'email', 'password', 'avatar') #
 
     def create(self, validated_data):
-        user = super().create(validated_data)
-        user.set_password(validated_data['password'])
+        # Tự tạo username nếu không có
+        username = validated_data.get('username') or validated_data['email'].split('@')[0]
+
+        user = User(
+            email=validated_data['email'],
+            username=username,
+            avatar=validated_data.get('avatar')
+        )
+        user.set_password(validated_data['password'])  # hash password
         user.save()
         return user
+
+class UserPreferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TravelPreference
+        fields = ['travel_type', 'location']
--- travel_project/users/urls.py
+++ travel_project/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, save_preferences, get_preferences
 
 urlpatterns = [
     ## auth/register
     path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view())
+    path('login', LoginView.as_view()),
+    # path('preferences', save_preferences, name='save_preferences'),
+    path('preferences', get_preferences, name='get_preferences'),   # GET
+    path('preferences/save', save_preferences, name='save_preferences'), # POST
 ]
